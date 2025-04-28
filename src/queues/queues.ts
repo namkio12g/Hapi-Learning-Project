ad5a94a5fa5eb8f19dc4777b4a5ad9186c5b6310
Klaus <102468487+namkio12g@users.noreply.github.com>
2025-04-28 23:08:09 +0700

    Update queues.ts

--- src/queues/queues.ts
+++ src/queues/queues.ts
@@ -4,10 +4,13 @@ import EnvConfig from "../config/envConfig";
 if (!EnvConfig.redisUri) {
   throw new Error("REDIS_URL is not defined");
 }
+//--------------Create a queue for sending mails------------------///
 const sendingVoucherByEmailQueue = new Queue(
   "checking-voucher-queue",
   EnvConfig.redisUri
 );
+//--------------Create a queue for processing editable eventse------------------///
+
 const checkingEditEventQueue = new Queue(
   "checking-edit-event-queue",
   EnvConfig.redisUri
