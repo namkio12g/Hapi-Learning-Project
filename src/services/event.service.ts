cee479f6451849f949c225150d3a0ee9a653f503
Klaus <102468487+namkio12g@users.noreply.github.com>
2025-04-28 22:52:43 +0700

    Update event.service.ts

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -17,7 +17,7 @@ export const EventService = {
   async createEvent(eventInfo: IEventDocument) {
     if (eventInfo.timeEnd && new Date(eventInfo.timeEnd) < new Date()) {
       throw Boom.badRequest("The end time cannot be before the start time");
-    }
+    }// checking if the end time of new event is before the start time 
 
     try {
       const newEvent = new EventModel(eventInfo);
@@ -61,14 +61,14 @@ export const EventService = {
 //-----------Update events' infomation.-----------///
   async updateEvent(eventId: string, eventInfo: Partial<IEventDocument>) {
     const eventObj = await EventModel.findById(eventId);
-    if (!eventObj) throw Boom.notFound("event not found");
+    if (!eventObj) throw Boom.notFound("event not found");// checking if event is existed
     const timeStart = eventObj.timeStart;
     if (
       eventInfo.timeEnd &&
       timeStart &&
       new Date(eventInfo.timeEnd) < timeStart
     )
-      throw Boom.badRequest("The end time cannot be before the start time");
+      throw Boom.badRequest("The end time cannot be before the start time");// checking the end time of the event
 
     try {
       const updatedEventObj = await EventModel.findByIdAndUpdate(
@@ -100,26 +100,30 @@ export const EventService = {
   
 //-----------Request a new voucher of a event .-----------///
   async requestNewVoucher(voucherInfo: IRequestNewVoucherInfo) {
-    const code = randomBytes(4).toString("hex");
+    const code = randomBytes(4).toString("hex"); // create a new code for a new voucher
     try {
+      // use retry transaction function if any collision happened
       const result = (await retryTransaction(async function createNewVoucher(
         session: ClientSession
       ): Promise<IVoucherDocument> {
+        // find all vouchers of the event in Database to check the sum of them
         const eventObj = await EventModel.findById(voucherInfo.eventId).session(
           session
         );
+        // check if the event is not existed
         if (!eventObj) throw Boom.notFound("Event not found");
         const existedVoucher = await VoucherModel.find(
           { eventApplied: voucherInfo.eventId },
           null,
-          { session, readPreference: "primary" }
+          { session, readPreference: "primary" } // read the primary node of the Database
         );
+        // if the number of issued vouchers exceeds the event's maximum quantity , return the 456 error code
         if (existedVoucher.length >= eventObj.maxVoucherQuantity)
           throw Boom.boomify(
             new Error("The event has reached its maximum number of vouchers"),
             { statusCode: 456 }
           );
-
+      // if not , create a new voucher and return the data
         const voucher = new VoucherModel({
           eventApplied: voucherInfo.eventId,
           code: code,
